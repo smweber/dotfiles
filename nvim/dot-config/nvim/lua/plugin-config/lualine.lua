370bd17b32d89713aecfd32371d9c35a4ea57666
Scott Weber <scott@r45.ca>
2026-03-25 14:03:48 -0400

    use catppuccin-nvim lualine theme

--- nvim/dot-config/nvim/lua/plugin-config/lualine.lua
+++ nvim/dot-config/nvim/lua/plugin-config/lualine.lua
@@ -2,7 +2,7 @@
 require('lualine').setup({
   options = {
     icons_enabled = true,
-    theme = 'catppuccin',
+    theme = 'catppuccin-nvim',
     component_separators = { left = '|', right = '|' },
     section_separators = { left = '', right = '' },
     disabled_filetypes = {
